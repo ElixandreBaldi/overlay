1c79a3647e134a54a273d86b08c5ddaa53c90b99
Elixandre <elixandre_michael@hotmail.com>
2018-10-09 16:56:22 -0300

    put aparentemente pronto

--- src/overlay/vcube/VCubeInitializer.java
+++ src/overlay/vcube/VCubeInitializer.java
@@ -5,10 +5,43 @@
  */
 package overlay.vcube;
 
+import java.util.Random;
+import peersim.config.Configuration;
+import peersim.core.Network;
+import peersim.core.Node;
+import peersim.dynamics.NodeInitializer;
+
 /**
  *
  * @author elixandrebaldi
  */
-public class VCubeInitializer {
-    
+public class VCubeInitializer implements NodeInitializer {
+
+	private static final String PAR_PROT = "protocol";
+
+	private int pid = 0;
+
+	private VCubeProtocol vcp;
+
+	public VCubeInitializer(String prefix) {
+		pid = Configuration.getPid(prefix + "." + PAR_PROT);                
+	}
+
+	public void initialize(Node n) {
+		vcp = (VCubeProtocol) n.getProtocol(pid);
+		join(n);
+	}
+
+	public void join(Node myNode) {
+		Random generator = new Random();
+		                
+		// search a node to join
+		Node n;
+		do {
+                    n = Network.get(generator.nextInt(Network.size()));
+		} while (n != null || n.isUp() != false);
+                
+                System.out.println("Teste: "+n.getIndex());
+		
+	}
 }
